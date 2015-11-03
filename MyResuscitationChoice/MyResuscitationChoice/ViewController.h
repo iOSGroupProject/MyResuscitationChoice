7bf3c35d53ccba511f32bcc98158a88c5606a5d9
carlin010 <carlin010@knights.gannon.edu>
2015-11-03 15:33:03 -0500

    Added Backend UI

--- MyResuscitationChoice/MyResuscitationChoice/ViewController.h
+++ MyResuscitationChoice/MyResuscitationChoice/ViewController.h
@@ -10,6 +10,7 @@
 
 @interface ViewController : UIViewController
 
+- (IBAction)addNewRecord:(id)sender;
 
 @end
 
--- MyResuscitationChoice/MyResuscitationChoice/ViewController.m
+++ MyResuscitationChoice/MyResuscitationChoice/ViewController.m
@@ -24,4 +24,8 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)addNewRecord:(id)sender {
+    [self performSegueWithIdentifier:@"idSegueEditInfo" sender:self];
+}
+
 @end
