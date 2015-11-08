384811aca44db5ff57fb8f47a4300375ee9bd1c3
Nakka, Abhilash <nakka001@macpc1.local>
2015-11-08 18:39:09 -0500

    Frame Work is designed and able to register . Still working on setting up some parameters for the fields.

--- SignUpPage/ViewController.h
+++ SignUpPage/ViewController.h
@@ -10,6 +10,20 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIButton *registerBtn;
+@property (weak, nonatomic) IBOutlet UIButton *loginBtn;
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordFiled;
+@property (weak, nonatomic) IBOutlet UITextField *reEnterPwd;
+@property (weak, nonatomic) IBOutlet UITextField *primaryFirstName;
+@property (weak, nonatomic) IBOutlet UITextField *primaryLastName;
+@property (weak, nonatomic) IBOutlet UITextField *emailID;
+@property (weak, nonatomic) IBOutlet UITextField *phoneNumber;
+@property (weak, nonatomic) IBOutlet UITextField *dateOfBirth;
+
+
+- (IBAction)registerUser:(id)sender;
+
 
 @end
 
